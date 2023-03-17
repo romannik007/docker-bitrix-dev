6ff621b848c8c6c05a7f70379c26a7976e81e454
roman <roman@home>
2023-03-17 12:48:31 +0300

    Update .gitignore, compose.yml, and 2 more files...

--- services/web/entrypoint.sh
+++ services/web/entrypoint.sh
@@ -10,6 +10,9 @@ mkdir -p /tmp/php_sessions && chmod -R 777 /tmp/php_sessions
 mkdir -p /tmp/php_upload && chmod -R 777 /tmp/php_upload
 mkdir -p /home/bitrix/.bx_temp/sitemanager
 chmod -R 777 /home/bitrix/.bx_temp/sitemanager
+chown www-data:www-data /var/www
+ln -s /user/.ssh /var/www/.ssh 
+chown www-data:www-data /var/www/.ssh
 cd /etc/apache2/mods-available/ && a2enmod * 
 cd /etc/apache2/mods-available/ && a2dismod dav_fs dav dav_lock mpm_worker
 exec /usr/bin/supervisord
